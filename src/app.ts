5b149258ead17dbf6c66075d421e96b634e77799
Alejo Saez <alejo.gebicki@gmail.com>
2024-08-20 19:57:10 -0300

    install cors

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import userRouter from "./routes/userRoute";
 import productRouter from "./routes/productsRoute";
 import categoryRouter from "./routes/categoriesRoute";
 import { errorHandler } from "./middlewares/errorHandler";
 import sizesRouter from "./routes/sizeRoute";
 import combinationRouter from "./routes/combinationRoute";
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+// Configuración de CORS para aceptar peticiones de cualquier origen
+app.use(cors({
+  origin: '*',  // Permite todos los orígenes
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: false,  // Desactiva el envío de cookies con la solicitud
+}));
 
 app.use("/api", userRouter);
 app.use("/api", productRouter);
